6dfa6866b4a359032f8be33be0c37a10bb91230d
Anmol Singh <anmolsingh1597@gmail.com>
2020-05-30 15:21:16 -0400

    Anmol Singh: javascript

---
+++ scripts/myFunctions.js
@@ -0,0 +1,21 @@
+// functions list
+
+//thank you alert
+function tYAlert(){
+    alert("Thank You for interest in Fitness Club. We will contact you soon.")
+}
+
+//Burpee Window
+function burpees(){
+    window.open("scripts/burpees.html", "_blank", "width:610 , height:360")
+}
+
+//Plank window
+function plank(){
+    window.open("scripts/plank.html", "_blank", "width:610 , height:360")
+}
+
+//Plank window
+function mtnClimber(){
+    window.open("scripts/mtn.html", "_blank")
+}
